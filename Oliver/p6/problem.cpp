2438660a73c2d3241c871f8f341d5058d15106ff
Oliver Lyon <ol@Olivers-MBP.hitronhub.home>
2021-12-14 02:45:04 -0600

    closed form sol

--- Oliver/p6/problem.cpp
+++ Oliver/p6/problem.cpp
@@ -6,11 +6,7 @@ int main(){
     int n = 100;
     long int sol = 0;
     int sqr_sum = (n*(n+1)*n*(n+1))/4;
-    int sum_sqr = 0;
-
-    for(int i=1; i <= n; i++){
-        sum_sqr += i*i;
-    }
+    int sum_sqr = (n*(n+1)*(2*n+1)/6);
 
     sol = sqr_sum - sum_sqr;
 
