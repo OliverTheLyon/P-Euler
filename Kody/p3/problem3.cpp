57bf93d4252725e20d5fdeb45db29b41b5ac7cd7
teddy2021 <darkking@live.ca>
2021-12-18 00:51:27 -0500

    Updated problem 3 to successfully find the correct result in reasonable time

--- Kody/p3/problem3.cpp
+++ Kody/p3/problem3.cpp
@@ -1,39 +1,25 @@
 #include <iostream>
+#include <algorithm>
 #include "common-lib.hpp"
 using namespace std;
 
-long int * factor(long int n){
-	long int * res;
-	long int * values = (long int *)malloc(sizeof(long int) * 1);
-	*values = 0;
-	for(long int i = 2; i < n; i += 1){
+long int factor(long int n, long int div){
+	if( 0 == div ){
+		div = 2;
+	}
+	for(int i = div; i < n; i += 1){
 		if(n % i == 0){
-			res = factor(n/i);
-			values = (long int *)realloc((void*)values, sizeof(long int) * 3);
-			*values = 2;
-			values[1] = i;
-			values[2] = n/i;
-			for(int i = 1; i < *res; i += 1){
-				values = (long int *)realloc((void*)values, sizeof(long int) * ((*values) * 2));
-				*values += 1;
-				values[*values] = res[i];
-				break;
-			}
+			return max((long int)i, factor(n/i, div));
 		}
 	}
-	return values;
+	return n;
 
 }
 
 
+
 int main(){
-	long int * res = factor(600851475143);
-	long int top = 0;
-	for(long int i = 1; i < *res; i += 1){
-		if(AKSprime(res[i]) && res[i] > top){
-			top = res[i];
-		}
-	}
-	cout << endl << "The largest prime factor of 600851475143 is " << top << endl;
+	long int res = factor(600851475143, 0);
+	cout << endl << "The largest prime factor of 600851475143 is " << res << endl;
 	return 0;
 }
